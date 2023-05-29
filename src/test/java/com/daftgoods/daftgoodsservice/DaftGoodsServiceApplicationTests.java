ad728e300fc18b670ede36120c0c716a2385d792
Liam <willis8437@saskpolytech.ca>
2023-05-29 18:55:11 +0000

    Trying to make and run a jar

--- src/test/java/com/daftgoods/daftgoodsservice/DaftGoodsServiceApplicationTests.java
+++ src/test/java/com/daftgoods/daftgoodsservice/DaftGoodsServiceApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class DaftGoodsServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+//	@Test
+//	void contextLoads() {
+//	}
 
 }
